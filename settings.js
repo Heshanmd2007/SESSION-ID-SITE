d53afaf7145d5e9475484330f6d6030f6b9d3e49
Heshanmd2007 <heshanmd2007@gmail.com>
2025-04-07 22:14:54 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    PREFIX: "HESHAN-MD~",
+    CONNECTED_MESSAGE: `
+┏━━━━━━━━━━━━━━
+┃HESHAN MD SESSION IS 
+┃SUCCESSFULLY
+┃CONNECTED ✅🔥
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬`
+};
